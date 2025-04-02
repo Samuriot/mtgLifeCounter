1676acb10a0b8894d217e05bb798e03f02460963
Samuriot <92127962+Samuriot@users.noreply.github.com>
2025-04-01 17:02:14 -0700

    changed background color + created alertDialogue.dart

--- lib/components/lifeTile.dart
+++ lib/components/lifeTile.dart
@@ -3,7 +3,8 @@
 import 'package:flutter/material.dart';
 
 class LifeTile extends StatefulWidget {
-  const LifeTile({super.key});
+  final String commander;
+  const LifeTile({super.key, required this.commander});
 
   @override
   State<LifeTile> createState() => _LifeTileState();
@@ -11,6 +12,7 @@ class LifeTile extends StatefulWidget {
 
 class _LifeTileState extends State<LifeTile> {
   int life = 40;
+  int commanderDmg = 0;
   void incrementCounter() {
 		// setState rebuilds the widget to reflect changes
 		setState(() {
@@ -23,6 +25,47 @@ class _LifeTileState extends State<LifeTile> {
 			life--;
 		});
 	}
+  bool isDead() {
+    if(life <= 0) {
+      return true;
+    } else if(commanderDmg >= 21) return true;
+    return false;
+  }
+
+  Widget renderWidget() {
+    if(isDead()) {
+      return Text("You Fucking Suck");
+    } 
+    return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: decrementCounter, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 55, 30, 97)
+              ),
+              child: Icon(Icons.remove)
+            ),
+            Column(
+              children: [
+                Text(
+                  widget.commander, style: TextStyle(fontSize: 40)
+                ),
+                Text(
+                  life.toString(), style: TextStyle(fontSize: 40)
+                ),
+              ],
+            ),
+            ElevatedButton(
+              onPressed: incrementCounter, 
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 55, 30, 97)
+              ),
+              child: Icon(Icons.add)
+            ),
+          ],
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,28 +77,7 @@ class _LifeTileState extends State<LifeTile> {
           color: Colors.deepPurple,
           borderRadius: BorderRadius.circular(12),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            ElevatedButton(
-              onPressed: decrementCounter, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 55, 30, 97)
-              ),
-              child: Icon(Icons.remove)
-            ),
-            Text(
-              life.toString(), style: TextStyle(fontSize: 40)
-            ),
-            ElevatedButton(
-              onPressed: incrementCounter, 
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 55, 30, 97)
-              ),
-              child: Icon(Icons.add)
-            ),
-          ],
-        ),
+        child: renderWidget()
       ),
     );
   }
--- lib/pages/counterPage.dart
+++ lib/pages/counterPage.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:magicapp/components/lifeTile.dart';
 
 // Stateful Widget updates the value on screen
 class CounterPage extends StatefulWidget {
-	const CounterPage({super.key});
+	const CounterPage({
+    super.key
+  
+  });
 
 	@override
 	State<CounterPage> createState() => _CounterPageState();
@@ -11,36 +16,44 @@ class CounterPage extends StatefulWidget {
 
 class _CounterPageState extends State<CounterPage> {
 
-	// variable
-	int counter = 0;
+	// variables
+	int numPlayers = 4;
 
-	// method
-	void incrementCounter() {
-		// setState rebuilds the widget to reflect changes
-		setState(() {
-			counter++;
-		});
+	List<Widget> renderPlayers() {
+    List<Widget> players = [];
+		for(int i = 0; i < numPlayers; i++) {
+			players.insert(i, const LifeTile(commander: "The Wise Mothman",));
+		}
+		return players;
 	}
 
-	void decrementCounter() {
-		setState(() {
-			counter--;
-		});
-	}
+	List<int> diceRoll() {
+		List<int> rollCounts = [];
+		for(int i = 0; i < numPlayers; i++) {
+			rollCounts.insert(i, Random().nextInt(21) + 1);
+		}
 
+    int winner = 0;
+    int maxRoll = rollCounts[0];
+    for(int i = 1; i < numPlayers; i++) {
+      if(maxRoll < rollCounts[i]) {
+        winner = i;
+        maxRoll = rollCounts[i];
+      }
+    }
+
+		return [winner, maxRoll];
+	}
+  
 	// UI 
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 51, 49, 49),
 			body: Center(
 				child: Column(
 					mainAxisAlignment: MainAxisAlignment.center,
-					children: [
-						LifeTile(),
-            LifeTile(),
-            LifeTile(),
-            LifeTile(),
-					]
+					children: renderPlayers(),
 				)
 			)
 		);
